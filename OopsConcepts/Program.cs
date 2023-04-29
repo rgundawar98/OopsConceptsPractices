cbe629f5f73222353ddf1c300ef34d9b5068927a
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-04-29 16:12:45 +0530

    [Rahul]Adding welcome message

---
+++ OopsConcepts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OopsConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Oops Concepts");
+        }
+    }
+}
