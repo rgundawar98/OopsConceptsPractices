82f086f1c4d81e985a4a4a0550170a8b244f86a5
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-04-29 16:24:56 +0530

    [Rahul]Program for ClassAndObject

---
+++ OopsConcepts/ClassAndObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts
+{
+    //Class is a blueprint of program
+    //Class contains methods,variables,constructors etc.
+    public class ClassAndObject 
+    {
+        int id = 10; //Instance variables
+        public void Demo()  //Method is entity of class 
+        {
+            Console.WriteLine("This is a non Parametrized method");
+        }
+        public ClassAndObject()
+        {
+            Console.WriteLine("It is default constructor");
+        }
+    }
+}
--- OopsConcepts/Program.cs
+++ OopsConcepts/Program.cs
@@ -7,6 +7,17 @@ namespace OopsConcepts
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Oops Concepts");
+            Console.WriteLine("Choose the option from below");
+            Console.WriteLine("1.Class And object");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    //Object created for  ClassAndObject Class as classAndObject
+                    ClassAndObject classAndObject = new ClassAndObject();
+                    classAndObject.Demo();
+                    break;
+            }
         }
     }
 }
