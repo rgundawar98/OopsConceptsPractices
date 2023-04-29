9c17c19550bb6c1b9dabbfa355e3fcf395786829
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-04-29 16:38:13 +0530

    [Rahul]Program using inheritance

---
+++ OopsConcepts/Inheritance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts
+{
+    //Inheritance means one class can acquire all properties and methods of another class
+    public class Mustang  
+    {
+        public void Car()
+        {
+            Console.WriteLine("This is Expensive Car");
+        }
+    }
+     
+    class Gear: Mustang   //Gear class is inherited with Mustang class
+    {
+        public void Gear_System()
+        {
+            Console.WriteLine("Mustang having Automatic Gear System");
+        }
+    }
+}
--- OopsConcepts/Program.cs
+++ OopsConcepts/Program.cs
@@ -8,7 +8,7 @@ namespace OopsConcepts
         {
             Console.WriteLine("Welcome to the Oops Concepts");
             Console.WriteLine("Choose the option from below");
-            Console.WriteLine("1.Class And object");
+            Console.WriteLine("1.Class And object\n2.Inheritance");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -16,6 +16,14 @@ namespace OopsConcepts
                     //Object created for  ClassAndObject Class as classAndObject
                     ClassAndObject classAndObject = new ClassAndObject();
                     classAndObject.Demo();
+                    break;
+                case 2:
+                    Mustang mustang = new Mustang();  //Parent Class
+                    mustang.Car(); //It call its own method using reference object
+                    Gear gear = new Gear(); //Child class or inherited class
+                    //Child class can call its method and parent class method too
+                    gear.Gear_System();
+                    gear.Car();
                     break;
             }
         }
